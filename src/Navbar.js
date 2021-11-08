4504639cca5d261078205ec0fbdd1290273d123d
rizkygusna <rizki.farid1998@gmail.com>
2021-11-08 11:49:14 +0700

    Add Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,18 @@
+import logo from './assets/logo.png';
+import user from './assets/user.png';
+
 const Navbar = () => {
   return (
     <header>
-      <div class='container row'>
-        <a class='logo' href=''>
-          <img src='./img/logo.svg' alt='conquering responsive logo' />
+      <div className='container row'>
+        <a className='logo' href='#'>
+          <img src={logo} alt='conquering responsive logo' />
         </a>
         <nav className='nav'>
           <p>Admin</p>
-          <img src='' alt='user-icon' />
+          <a href=''>
+            <img src={user} alt='user icon' />
+          </a>
         </nav>
       </div>
     </header>
