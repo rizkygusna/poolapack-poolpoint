2ec5173a4a901d3476da13cecf2f17b694c0435c
rizkygusna <gusnafarid@gmail.com>
2021-11-14 18:30:30 +0700

    Add layout to history entry

--- src/History.js
+++ src/History.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 
 import coin from './assets/coin.png';
+import referal from './assets/referal.png';
+import cashback from './assets/cashback.png';
 
 const History = () => {
   const [value, onChange] = useState([new Date(), new Date()]);
@@ -45,17 +47,28 @@ const History = () => {
       </div>
       {transactions.map((data) => (
         <div className='history-entry'>
-          <p>
+          <p className='bold'>
             {data.date} <span>{data.time}</span>
           </p>
-          <h3>{data.type}</h3>
-          <h3>{data.amount}</h3>
-          <p>
-            <span>
-              <img src={coin} alt='' />
-            </span>
-            Poolpoint
-          </p>
+          <div className='history-entry-details'>
+            <h3 className='details-type'>
+              <span>
+                {data.type === 'Referral' && <img src={referal} alt='referal icon' />}
+                {data.type === 'Cashback' && <img src={cashback} alt='cashback icon' />}
+              </span>
+              {data.type}
+            </h3>
+            <div className='details-amount'>
+              <h3>{data.amount}</h3>
+              <p>
+                <span>
+                  <img src={coin} alt='small coin icon' />
+                </span>
+                Poolpoint
+              </p>
+            </div>
+          </div>
+          <hr />
         </div>
       ))}
     </section>
